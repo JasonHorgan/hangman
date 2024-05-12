3a9ee0d1baafcf319fa6061b0b068e92cee75b22
Jason Horgan <jasonahorgan@gmail.com>
2024-05-12 12:31:57 +0000

    import random library

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
+import random 
+
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 #defining username input and welcome text
+
+word = ["pause" , "train" , "their" , "chair" , "press"]
+
 username = input('Welcome to Hangman! Please enter your username: ')
 print("Hello, " +username + "! Good luck in the game!")
