54bdb8b5d2f183901cb3d3475f24b405d18ddbdb
Jason Horgan <jasonahorgan@gmail.com>
2024-05-12 12:06:58 +0000

    Initial commit and add a username input for the player

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+#defining username input and welcome text
+username = input('Welcome to Hangman! Please enter your username: ')
+print("Hello, " +username + "! Good luck in the game!")
