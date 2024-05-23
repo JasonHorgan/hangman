4f0bcac8b2a0329df72aa5e29d60a46ff835cf7d
Jason Horgan <jasonahorgan@gmail.com>
2024-05-23 21:15:31 +0000

    Ran code through linter to make sure there were no errors, then pasted updated code back into gitpod

--- run.py
+++ run.py
@@ -1,13 +1,10 @@
+import random
 import colorama
 from colorama import Fore, Back, Style
 colorama.init(autoreset=True)
-import random 
-
-#Variables
-
+# Variables
 STARTING_LIVES = 5
-
-word = ["pause" , "train" , "their" , "chair" , "press"]
+word = ["pause", "train", "their", "chair", "press"]
 
 
 def how_to_play():
@@ -20,78 +17,70 @@ def how_to_play():
     print("If your guess is not in the word, you lose a life\n")
     print("Keep making guesses until you get the word or run out of lives\n")
     print("Have fun!!!\n")
-    valid_choices = ('1' , '2')
-
+    valid_choices = ("1", "2")
     while True:
         start = input("Type 1 to start the game or 2 to quit \n")
-
         if start not in valid_choices:
-                print("Invalid choice, enter 1 or 2 \n")
-        else: 
-            break 
-
-    if start == '1':
-                play_game()
-    elif start == '2':
-                print('See you next time! ')
+            print("Invalid choice, enter 1 or 2 \n")
+        else:
+            break
+    if start == "1":
+        play_game()
+    elif start == "2":
+        print("See you next time! ")
 
 
 def main_menu():
     print("Welcome to the word guess game! \n")
-    global username 
-    username = input("Please enter your username: \n") 
-
-    valid_choices = ('1' , '2' , '3')
-
+    global username
+    username = input("Please enter your username: \n")
+    valid_choices = ("1", "2", "3")
     while True:
-        start = input("Type 1 to start the game, 2 to see instructions or 3 to quit \n")
-
+        start = input("Type 1 to start game, 2 for instructions, 3 to quit \n")
         if start not in valid_choices:
-                print(Fore.RED + "Invalid choice, enter 1 or 2 \n")
-        else: 
-            break 
+            print(Fore.RED + "Invalid choice, enter 1 or 2 \n")
+        else:
+            break
+    if start == "1":
+        play_game()
+    elif start == "2":
+        how_to_play()
+    elif start == "3":
+        print("See you next time! ")
 
-    if start == '1':
-                play_game()
-    elif start == '2':
-                how_to_play()
-    elif start =='3':
-        print('See you next time! ')
-    
 
-def check_letter(secret_word , guess):
+def check_letter(secret_word, guess):
     print(Fore.GREEN + guess + " is in the word!")
-    print("The letter was found at position " + str(secret_word.find(guess)+1))
+    print("Letter was found at position " + str(secret_word.find(guess) + 1))
+
 
 def get_secret_word():
     secret_word = random.choice(word)
     return secret_word
 
-def you_win(secret_word , guess , lives):
+
+def you_win(secret_word, guess, lives):
     if len(guess) == 5 and guess in secret_word:
-            print(Fore.GREEN + "Congrats, you won! \n")
-            print("The word was " + Fore.GREEN + secret_word)
-            user_score = lives*50
-            print("You finished with " + str(lives) + " lives remaining \n")
-            print("You scored " + Fore.YELLOW + str(user_score) + " points \n") 
+        print(Fore.GREEN + "Congrats, you won! \n")
+        print("The word was " + Fore.GREEN + secret_word)
+        user_score = lives * 50
+        print("You finished with " + str(lives) + " lives remaining \n")
+        print("You scored " + Fore.YELLOW + str(user_score) + " points \n")
+
 
 def restart():
-        print("Would you like to play again? \n")
-
-        valid_choices = ('Y' , 'N')
-
-        while True:
-            start = input("Press Y to play again or N to quit\n").upper()
-
-            if start not in valid_choices:
-                print(Fore.RED + "Invalid choice, enter Y or N \n")
-            else: 
-                break 
-
-        if start == 'Y':
-                play_game()
-        elif start == 'N':
-                print("Ok, see you next time! ")
+    print("Would you like to play again? \n")
+    valid_choices = ("Y", "N")
+    while True:
+        start = input("Press Y to play again or N to quit\n").upper()
+        if start not in valid_choices:
+            print(Fore.RED + "Invalid choice, enter Y or N \n")
+        else:
+            break
+    if start == "Y":
+        play_game()
+    elif start == "N":
+        print("Ok, see you next time! ")
 
 
 def play_game():
@@ -102,47 +91,38 @@ def play_game():
     lives = STARTING_LIVES
     secret_word = get_secret_word()
     guesses = []
-
     while lives > 0:
         print("number of lives remaining = " + str(lives))
-        
-
-        while True: 
+        while True:
             guess = input("Please guess a letter or word: \n").lower()
-
-            if len(guess) >=2 and len(guess) <=4:
-                print(Fore.RED + 'Guess can only be single letter or 5 letter word, try again\n')
-            
-            elif len(guess) >=6:
-                print('Guess must only be 5 letter in length \n')
+            if len(guess) >= 2 and len(guess) <= 4:
+                print(
+                    Fore.RED
+                    + "Guess can only be 1 or 5 letters in length: try again\n"
+                )
+            elif len(guess) >= 6:
+                print("Guess must only be 5 letter in length \n")
             elif not guess.isalpha():
                 print(Fore.RED + "Guess must only contain letters ")
-
             else:
-                 break
-
+                break
         if guess not in secret_word:
             print(Fore.RED + "Wrong! Guess another letter or word \n")
             guesses.append(guess)
-            lives -=1
+            lives -= 1
             print("Words guessed so far: " + " , ".join(guesses))
-
         elif len(guess) == 1 and guess in secret_word:
-            check_letter(secret_word , guess)
+            check_letter(secret_word, guess)
             guesses.append(guess)
             print("Words guessed so far: " + " , ".join(guesses))
         else:
-            you_win(secret_word , guess , lives)
+            you_win(secret_word, guess, lives)
             restart()
-
             break
-
     if lives == 0:
         print("Unlucky! The word was " + Fore.RED + secret_word)
         restart()
-       
 
 
 if __name__ == "__main__":
     main_menu()
-
